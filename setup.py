0ae7fab1f8ebe1d036e07b3cff4b2bf6641a97da
Mark Jones <mark@lunchtime.com.au>
2018-11-23 18:05:56 +1100

    update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'static/django_markdown/skins/simple/images/*.*',
         'templates/django_markdown/*.*',
     ]},
-    version = "0.3.384",
+    version = "0.3.385",
     description = "Django based bootstrap web framework",
     author = "Bart Jellema",
     author_email = "b@rtje.net",
