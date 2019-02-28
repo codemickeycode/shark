b3a6693f13ee4791e4fac2a5daeff854a72f268c
Mark Jones <mark@zibbet.com>
2019-02-28 14:23:51 +1100

    Updated gtm_code

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'static/django_markdown/skins/simple/images/*.*',
         'templates/django_markdown/*.*',
     ]},
-    version = "0.3.385",
+    version = "0.3.386",
     description = "Django based bootstrap web framework",
     author = "Bart Jellema",
     author_email = "b@rtje.net",
